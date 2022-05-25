0bd48e4ca22760e83ea1c12f627a9319cc3b484c
hasanboy <metinnazif77@gmail.com>
2022-05-26 01:29:12 +0500

    Create ProductTypeFilter

--- src/main/java/com/product/application/dto/hasanboy/AddressDto.java
+++ src/main/java/com/product/application/dto/hasanboy/AddressDto.java
@@ -1,4 +1,18 @@
 package com.product.application.dto.hasanboy;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class AddressDto {
+    private Integer id;
+
+    private String region;
+    private String city;
+    private String district;//Tuman
+    private String street;
+    private Integer home;
 }

e41db900672f39ab90881529308f7cc45a1e6e48
hasanboy <metinnazif77@gmail.com>
2022-05-26 01:33:06 +0500

    Update AddressDto

--- src/main/java/com/product/application/dto/hasanboy/AddressDto.java
+++ src/main/java/com/product/application/dto/hasanboy/AddressDto.java
@@ -4,15 +4,22 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class AddressDto {
     private Integer id;
-
+    @NotBlank(message = ("The region cannot be empty or null"))
     private String region;
+    @NotBlank(message = ("The region cannot be empty or null"))
     private String city;
+    @NotBlank(message = ("The region cannot be empty or null"))
     private String district;//Tuman
+    @NotBlank(message = ("The region cannot be empty or null"))
     private String street;
+    @Size(min = 3,max = 10,message = ("The number size of the house should be 3 to 10 times"))
     private Integer home;
 }

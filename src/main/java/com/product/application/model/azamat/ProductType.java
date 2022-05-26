28bec7993173ebe7b7424caca56e49456fc2d1e3
azamat <azamatnematov2003@gmail.com>
2022-05-26 17:56:30 +0500

    create productType, product

--- src/main/java/com/product/application/model/azamat/ProductType.java
+++ src/main/java/com/product/application/model/azamat/ProductType.java
@@ -1,4 +1,37 @@
 package com.product.application.model.azamat;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+
+@Entity
+@Table(name = ("productTypes"))
 public class ProductType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer rate;
+    private Boolean visible;
+    private Integer productType;
+    private Boolean status;
+
+
+    @Column(name = ("created_at"))
+    private LocalDateTime createdAt;
+
+    @Column(name = ("updated_at"))
+    private LocalDateTime updatedAt;
+
+    @Column(name = ("deleted_at"))
+    private LocalDateTime deletedAt;
 }

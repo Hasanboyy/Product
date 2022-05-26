273d5a2620e064f3bf7ad6da9cf8b7d060e42e43
azamat <azamatnematov2003@gmail.com>
2022-05-26 20:38:46 +0500

    create productTypeController

--- src/main/java/com/product/application/controller/azamat/ProductTypeController.java
+++ src/main/java/com/product/application/controller/azamat/ProductTypeController.java
@@ -1,4 +1,57 @@
 package com.product.application.controller.azamat;
 
+import com.product.application.dto.azamat.ProductDto;
+import com.product.application.dto.azamat.ProductTypeDto;
+import com.product.application.filter.azamat.ProductTypeFilter;
+import com.product.application.service.azamat.ProductTypeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/productTypes")
+@AllArgsConstructor
 public class ProductTypeController {
+    private final ProductTypeService productTypeService;
+
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody @Valid ProductTypeDto productTypeDto){
+        boolean result = productTypeService.create(productTypeDto);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/get{id}")
+    public ResponseEntity<?> get(@PathVariable("id") Integer id){
+        ProductDto result = productTypeService.get(id);
+        return ResponseEntity.ok(result);
+    }
+
+    @PutMapping("/update{id}")
+    public ResponseEntity<?> update(@PathVariable("id") Integer id,
+                                    @RequestBody @Valid ProductTypeDto productTypeDto){
+        boolean result = productTypeService.update(id, productTypeDto);
+        return ResponseEntity.ok(result);
+    }
+
+    @DeleteMapping("/delete{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Integer id){
+        boolean result = productTypeService.delete(id);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/pagenation")
+    public ResponseEntity<?> getAll(@RequestParam("s") Integer size,
+                                    @RequestParam("p") Integer page){
+        List<ProductTypeDto> result = productTypeService.findAllByPage(page, size);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/filter")
+    public ResponseEntity<?> filter(@RequestBody ProductTypeFilter productTypeFilter){
+        List<ProductTypeDto> result = productTypeService.filter(productTypeFilter);
+        return ResponseEntity.ok(result);
+    }
 }
--- src/main/java/com/product/application/service/azamat/ProductTypeService.java
+++ src/main/java/com/product/application/service/azamat/ProductTypeService.java
@@ -1,4 +1,40 @@
 package com.product.application.service.azamat;
 
+import com.product.application.dto.azamat.ProductDto;
+import com.product.application.dto.azamat.ProductTypeDto;
+import com.product.application.filter.azamat.ProductTypeFilter;
+import com.product.application.repository.azamat.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class ProductTypeService {
+
+    private ProductRepository productRepository;
+    public boolean create(ProductTypeDto productTypeDto) {
+        return false;
+    }
+
+    public ProductDto get(Integer id) {
+        return null;
+    }
+
+    public boolean update(Integer id, ProductTypeDto productTypeDto) {
+        return false;
+    }
+
+    public boolean delete(Integer id) {
+        return false;
+    }
+
+    public List<ProductTypeDto> findAllByPage(Integer page, Integer size) {
+        return null;
+    }
+
+    public List<ProductTypeDto> filter(ProductTypeFilter productTypeFilter) {
+        return null;
+    }
 }

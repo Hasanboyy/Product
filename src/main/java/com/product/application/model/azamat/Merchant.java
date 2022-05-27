beeac0e24dfd418c26ed733b39426dc05753f67c
azamat <azamatnematov2003@gmail.com>
2022-05-27 23:47:56 +0500

    create Merchant

--- src/main/java/com/product/application/model/azamat/Merchant.java
+++ src/main/java/com/product/application/model/azamat/Merchant.java
@@ -1,7 +1,30 @@
 package com.product.application.model.azamat;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+
+@Entity
+@Table(name = ("merchant"))
 public class Merchant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String name;
     private Boolean status;
+
+    @Column(name = ("created_at"))
+    private LocalDateTime createdAt;
+
+    @Column(name = ("updated_at"))
+    private LocalDateTime updatedAt;
+
+    @Column(name = ("deleted_at"))
+    private LocalDateTime deletedAt;
 }

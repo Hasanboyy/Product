951ecc1a2231a3ba7e7d36a45824c74945e89ff2
azamat <azamatnematov2003@gmail.com>
2022-05-28 00:40:34 +0500

    create  filtrlar azamatniki

--- src/main/java/com/product/application/filter/azamat/DivigatelFilter.java
+++ src/main/java/com/product/application/filter/azamat/DivigatelFilter.java
@@ -1,4 +1,13 @@
 package com.product.application.filter.azamat;
 
-public class DivigatelFilter {
+import com.product.application.filter.FilterDto;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DivigatelFilter extends FilterDto {
+    private String name;
+    private String surname;
+    private String direc;
 }
--- src/main/java/com/product/application/filter/azamat/MarchantFilter.java
+++ src/main/java/com/product/application/filter/azamat/MarchantFilter.java
@@ -1,4 +1,12 @@
 package com.product.application.filter.azamat;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MarchantFilter {
+    private String name;
+    private String surname;
+    private String direc;
 }

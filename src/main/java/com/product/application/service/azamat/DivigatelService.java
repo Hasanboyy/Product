f043d396301b449b2e1cce481fce4ef147abdc25
azamat <azamatnematov2003@gmail.com>
2022-05-28 00:20:55 +0500

    create controller service

--- src/main/java/com/product/application/service/azamat/DivigatelService.java
+++ src/main/java/com/product/application/service/azamat/DivigatelService.java
@@ -1,4 +1,33 @@
 package com.product.application.service.azamat;
 
+import com.product.application.dto.azamat.DivigatelDto;
+import com.product.application.filter.azamat.BrendFilter;
+import com.product.application.filter.azamat.DivigatelFilter;
+
+import java.util.List;
+
 public class DivigatelService {
+    public boolean create(DivigatelDto divigatelDto) {
+        return false;
+    }
+
+    public DivigatelDto get(Integer id) {
+        return null;
+    }
+
+    public boolean update(Integer id, DivigatelDto divigatelDto) {
+        return false;
+    }
+
+    public boolean delete(Integer id) {
+        return false;
+    }
+
+    public List<DivigatelDto> findAllByPage(Integer page, Integer size) {
+        return null;
+    }
+
+    public List<DivigatelDto> filter(DivigatelFilter divigatelFilter) {
+        return null;
+    }
 }

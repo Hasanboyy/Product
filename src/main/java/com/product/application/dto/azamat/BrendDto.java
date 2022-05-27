a57b1caeb9aebaf7d43039f431427e0d194dce00
azamat <azamatnematov2003@gmail.com>
2022-05-27 23:55:10 +0500

    create BrendDto DvigatelDto MerchantDto

--- src/main/java/com/product/application/dto/azamat/BrendDto.java
+++ src/main/java/com/product/application/dto/azamat/BrendDto.java
@@ -1,4 +1,18 @@
 package com.product.application.dto.azamat;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class BrendDto {
+    private Integer id;
+
+    @NotBlank(message = "The name cannot empty")
+    private String name;
+
 }

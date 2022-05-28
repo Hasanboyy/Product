53d1a0513d773d0a8147d317c9435bdd4a0ebcf9
azamat <azamatnematov2003@gmail.com>
2022-05-28 15:21:08 +0500

    azamat

---
+++ src/main/java/com/product/application/repository/azamat/ProductTypeRepository.java
@@ -0,0 +1,7 @@
+package com.product.application.repository.azamat;
+
+import com.product.application.model.azamat.ProductType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductTypeRepository extends JpaRepository<ProductType,Integer> {
+}
